8763e197401f0c1962101c011f7f057658a15298
hariharanragothaman <hariharanragothaman@gmail.com>
2020-04-28 17:29:26 -0400

    To find Length of longest uncommon subsequence

---
+++ String/0521_longest_uncommon_subsequence_1.py
@@ -0,0 +1,13 @@
+"""
+Q: Given 2 strings, find the longest uncommon subsequence
+
+Solution:
+This is just the length of the longest string in the I/P
+Initially had thought about uncommon characters and so on.
+"""
+def longest_uncommon_subsequence(string1, string2):
+    if string1 == string2:
+        return -1
+    else:
+        return max(len(string1), len(string2))
+    
