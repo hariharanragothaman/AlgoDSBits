97059459efb72752697c5ba209f0c4538066f0f1
hariharanragothaman <hariharanragothaman@gmail.com>
2020-04-28 13:41:42 -0400

    Reversing an integer - Perfect logic

---
+++ Math/0007_reverse_integer.py
@@ -0,0 +1,28 @@
+"""
+Given an integer - reverse it
+"""
+
+def reverse_integer(num):
+    sign = -1 if num < 0 else 1
+    result = 0
+
+    x = abs(num)
+
+    # Actual reversing logic
+    while x:
+        # Getting the LSB-  multiply by 10 every time; since it will be correct as we add digits
+        result = (x%10) + result*10
+        # Remove the LSB
+        x = x // 10
+
+
+    # Handle overflow bound scenario
+    if x > 2**31+1 or x < -2**31-1:
+        return 0
+
+    return result * sign
+
+number = 123
+number2 = -123
+result = reverse_integer(number2)
+print(result)
