661361a3a1f00c464abc6314b0c46d37cc4b412f
hariharanragothaman <hariharanragothaman@gmail.com>
2020-04-28 14:02:08 -0400

    CheatSheet update: most_common()-Counter & heapq for largest and smallest

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import heapq
 
 # To sort a hash-map by value or key
 hash_map = {}
@@ -9,3 +10,14 @@ inp_list = []
 ctr = Counter(inp_list)
 # This creates a tuple, and tuple are by default sorted by value, then we set the priority
 res = sorted(ctr, key= lambda x:(-ctr[x], x))
+
+# Getting the most_common  or 'frequent' elements- Returns the tuple
+arr = [1, 3, 5, 6, 7, 7, 1, 1, 2, 3]
+ctr1 = Counter(arr)
+top_three_frequent = ctr1.most_common(3)
+print(top_three_frequent)
+
+# Getting the top 'n' largest or top 'n' smallest
+marks = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+top_three_largest = heapq.nlargest(3, marks)
+top_three_smallest = heapq.nsmallest(3, marks
